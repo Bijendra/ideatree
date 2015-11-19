6032728949959492b0f7814b45494599f118a1c7
bijendra <bijendra.biju@gmail.com>
2015-11-19 05:48:29 -0800

    Revert "delete files"
    This reverts commit f60fc424bcd10f847e6342a97462bd387ec87998.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116165344) do
+ActiveRecord::Schema.define(version: 20151019174638) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -29,16 +29,12 @@ ActiveRecord::Schema.define(version: 20151116165344) do
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
 
   create_table "assignments", force: :cascade do |t|
-    t.string   "description",         limit: 255
-    t.integer  "user_id",             limit: 4
-    t.string   "category_id",         limit: 255
-    t.boolean  "status",                          default: false
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.string   "avatar_file_name",    limit: 255
-    t.string   "avatar_content_type", limit: 255
-    t.integer  "avatar_file_size",    limit: 4
-    t.datetime "avatar_updated_at"
+    t.string   "description", limit: 255
+    t.integer  "user_id",     limit: 4
+    t.string   "category_id", limit: 255
+    t.boolean  "status",                  default: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -47,12 +43,9 @@ ActiveRecord::Schema.define(version: 20151116165344) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string   "content",          limit: 255
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.string   "commentable_id",   limit: 255
-    t.string   "commentable_type", limit: 255
-    t.string   "user_id",          limit: 255
+    t.string   "content",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "likes", force: :cascade do |t|
@@ -82,10 +75,6 @@ ActiveRecord::Schema.define(version: 20151116165344) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.string   "avatar_file_name",       limit: 255
-    t.string   "avatar_content_type",    limit: 255
-    t.integer  "avatar_file_size",       limit: 4
-    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
